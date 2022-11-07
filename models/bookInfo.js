72a2cc8c1d44d14589c9053ca63d3acbe3de8661
pippen11 <111038332+pippen11@users.noreply.github.com>
2022-11-07 21:34:24 +0900

    221107
    221107

--- models/bookInfo.js
+++ models/bookInfo.js
@@ -2,31 +2,6 @@ const Sequelize = require("sequelize");
 
 module.exports = class BookInfo extends Sequelize.Model {
   static init(sequelize) {
-    return super.init(
-      {
-        title: {
-          type: Sequelize.STRING(255),
-        },
-        sub_title: {
-          type: Sequelize.STRING(255),
-        },
-        book_intro: {
-          type: Sequelize.STRING(255),
-        },
-        category: {
-          type: Sequelize.STRING(255),
-        },
-        publisher: {
-          type: Sequelize.STRING(255),
-        },
-      },
-      {
-        sequelize,
-        modelName: "",
-        tableName: "",
-        charset: "utf8mb4",
-        collate: "utf8mb4_general_ci",
-      }
-    );
+    return super.init({}, {});
   }
 };
