5bd577d6306433d3ca37720bc92ddfd0285e30f4
efforthye <111038259+efforthye@users.noreply.github.com>
2022-11-07 11:27:18 +0900

    Merge를 위한 임시저장

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.set("port", process.env.PORT || 8080);
 
-app.use("/", express.static(path.join(__dirname, "beforeMain/v3/myLibrary")));
+app.use("/", express.static(path.join(__dirname, "beforeMain/v3/login")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
