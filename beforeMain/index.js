37dc2e4f634f878b9b56eb9d5aaa601133fa1010
efforthye <efforthye@gmail.com>
2022-11-08 11:27:54 +0900

    로그인 여부에 따른 메인화면

--- beforeMain/index.js
+++ beforeMain/index.js
@@ -3,3 +3,10 @@ const loginbtn = document.getElementById("loginbtn");
 loginbtn.onclick = () => {
   location.href = "./v3/login";
 };
+
+let tempCookie = document.cookie.split("=");
+let cookieJwt = tempCookie[1];
+console.log(cookieJwt);
+
+// 만약 쿠키가 있으면 로그인 후 메인으로 보냄
+if(cookieJwt) location.href = "/v3/MainHome";
