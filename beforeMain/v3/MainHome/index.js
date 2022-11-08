1df420afef37de3edc19eb0b5d650b661e2af5d7
efforthye <efforthye@gmail.com>
2022-11-08 09:20:19 +0900

    Main Home 수정 및 추가
    날짜 : 2022. 11. 7.
    내용 : afterMain을 MainHome으로 변경하였고, html, css를 수정 및 js 기능들(로그인, 로그아웃 쿠키 처리 등)을 새로 만들었다.

--- beforeMain/v3/MainHome/index.js
+++ beforeMain/v3/MainHome/index.js
@@ -1,3 +1,4 @@
+//  inf ani
 let imgLoop1 = document.getElementById("obj-slide-box-004");
 let imgLoop2 = document.getElementById("obj-slide-box-005");
 let imgLoop3 = document.getElementById("obj-slide-box-006");
@@ -9,14 +10,12 @@ let ani3 = document.getElementsByClassName("animate3");
 imgLoop1.addEventListener("mouseover", () => {
   ani1[0].style.animationPlayState = "paused";
 });
-
 imgLoop1.addEventListener("mouseleave", () => {
   ani1[0].style.animationPlayState = "running";
 });
 imgLoop2.addEventListener("mouseover", () => {
   ani2[0].style.animationPlayState = "paused";
 });
-
 imgLoop2.addEventListener("mouseleave", () => {
   ani2[0].style.animationPlayState = "running";
 });
@@ -44,6 +43,7 @@ const onClick = (e) => {
     e.preventDefault();
   }
 };
+
 const onScrollStart = (e) => {
   startX = getClientX(e);
   window.addEventListener("mousemove", onScrollMove);
@@ -102,3 +102,63 @@ const onScrollEnd = (e) => {
     list.style.transition = "";
   }, 500);
 };
+
+
+
+
+// 여기서부터 혜림 2022.11.7.
+let tempCookie = document.cookie.split("=");
+let cookieJwt = tempCookie[1];
+console.log(cookieJwt);
+
+// 만약 쿠키가 있으면 쿠키정보 가져오는 함수 실행
+if(cookieJwt){
+  cookieVerify();
+}else{
+  // 쿠키 없으면 로그인 전 메인으로 보냄
+  location.href = "http://localhost:8080";
+}
+
+const nickname = document.getElementById("nickname");
+
+// 클라이언트쪽 js에서는 jwt.verify를 사용할 수 없기 때문에 post로 대충 보내준 다음
+// 거기에서 verify를 해준뒤 값을 받아온다.
+async function cookieVerify(){
+  const data = await axios.post("/v3/mainhome/cookieInfo", {cookieJwt});
+
+  console.log(data.data.status);
+  console.log(data.data.id);
+  console.log(data.data.name);
+  console.log(data.data.nickname);
+  console.log(data.data.publish);
+
+  // 만약 작가명 있으면 작가명, 작가명 없으면 이름을 메인에 띄워줌
+  if(data.data.nickname){
+    nickname.innerHTML = data.data.nickname;
+  }else{
+    nickname.innerHTML = data.data.name;
+  }
+
+}
+
+// 로그아웃 
+document.getElementById("logOut-btn").onclick = async() =>{
+
+  if(!cookieJwt){
+    alert("비정상적인 접근입니다. 삐뽀삐뽀");
+    location.href = "https://www.police.go.kr/index.do";
+    return;
+  }
+
+  const data = await axios.post("/v3/mainhome/clearCookie", {cookieName : tempCookie[0]});
+
+  if(data.data.status==200){
+    alert("로그아웃 성공");
+    location.href = "http://localhost:8080";
+  }else{
+    alert("로그아웃 실패");
+  }
+}
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const join = require("./join.js");
 const login = require("./login.js");
 const category = require("./category.js");
+const mainhome = require("./mainhome.js");
 
 router.use("/join", join);
 router.use("/login", login);
 router.use("/category", category);
+router.use("/mainhome", mainhome);
 
 module.exports = router;
