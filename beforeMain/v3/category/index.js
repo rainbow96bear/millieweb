41b45a4380504d1c5bb79603f1f28c88bd17f16f
efforthye <efforthye@gmail.com>
2022-11-08 10:50:40 +0900

    내 서재 작업 시작

--- beforeMain/v3/category/index.js
+++ beforeMain/v3/category/index.js
@@ -3,11 +3,12 @@ const search_btn = document.getElementById("search_btn");
 const my_book_btn = document.getElementById("my_book_btn");
 
 logo.onclick = () => {
-  location.href = "../afterMain";
+  location.href = "../MainHome";
 };
 search_btn.onclick = () => {};
 my_book_btn.onclick = () => {
   alert("나중에 내 서재로 이동하도록 할 겁니다.");
+  location.href = "../myLibrary";
 };
 const item = document.getElementsByClassName("item");
 
