918648554aeeb721136a800e2b2c49915663a0ad
efforthye <efforthye@gmail.com>
2022-11-09 20:11:39 +0900

    Main Home에 닉네임 추가

--- beforeMain/v3/MainHome/index.js
+++ beforeMain/v3/MainHome/index.js
@@ -237,3 +237,66 @@ function myFunction6() {
   document.getElementById("fri").classList.add("off");
   document.getElementById("wed").classList.add("off");
 }
+
+
+
+// 여기서부터 혜림 2022.11.7.
+let tempCookie = document.cookie.split("=");
+let cookieJwt = tempCookie[1];
+console.log(cookieJwt);
+
+// 만약 쿠키가 있으면 쿠키정보 가져오는 함수 실행
+if (cookieJwt) {
+  cookieVerify();
+} else {
+  // 쿠키 없으면 로그인 전 메인으로 보냄
+  location.href = "http://localhost:8080";
+}
+
+const nickname = document.getElementById("nickname");
+
+// 클라이언트쪽 js에서는 jwt.verify를 사용할 수 없기 때문에 post로 대충 보내준 다음
+// 거기에서 verify를 해준뒤 값을 받아온다.
+async function cookieVerify() {
+  const data = await axios.post("/v3/mainhome/cookieInfo", { cookieJwt });
+
+  console.log(data.data.status);
+  console.log(data.data.id);
+  console.log(data.data.name);
+  console.log(data.data.nickname);
+  console.log(data.data.publish);
+
+  // 만약 작가명 있으면 작가명, 작가명 없으면 이름을 메인에 띄워줌
+  if (data.data.nickname) {
+    nickname.innerHTML = data.data.nickname;
+  } else {
+    nickname.innerHTML = data.data.name;
+  }
+}
+
+// 로그아웃
+document.getElementById("logOut-btn").onclick = async () => {
+  if (!cookieJwt) {
+    alert("비정상적인 접근입니다. 삐뽀삐뽀");
+    location.href = "https://www.police.go.kr/index.do";
+    return;
+  }
+
+  const data = await axios.post("/v3/mainhome/clearCookie", {
+    cookieName: tempCookie[0],
+  });
+
+  if (data.data.status == 200) {
+    alert("로그아웃 성공");
+    location.href = "http://localhost:8080";
+  } else {
+    alert("로그아웃 실패");
+  }
+};
+
+// 여기서부터 혜림 2022.11.8.
+const myLibraryBtn = document.getElementById("myLibraryBtn");
+
+myLibraryBtn.onclick = () => {
+  location.href = "../myLibrary";
+};
