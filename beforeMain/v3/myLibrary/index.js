89f073e3a92bc6fb354716653e1dff0a3a6b157e
efforthye <efforthye@gmail.com>
2022-11-09 14:01:39 +0900

    책 append 준비 완료

--- beforeMain/v3/myLibrary/index.js
+++ beforeMain/v3/myLibrary/index.js
@@ -9,24 +9,64 @@ if(cookieJwt){
   location.href = "http://localhost:8080";
 }
 
+// 작가인지 일반 유저인지에 따라 다른 정보를 띄움
 const userName = document.getElementById("userName");
 const contentHeader = document.getElementById("contentHeader");
 
 async function cookieVerify(){
   const data = await axios.post("/v3/mainhome/cookieInfo", {cookieJwt});
 
-  console.log(data.data.status);
-  console.log(data.data.id);
-  console.log(data.data.name);
-  console.log(data.data.nickname);
-  console.log(data.data.publish);
-
   if(data.data.nickname){
     userName.innerHTML = `<span class="nameBold">${data.data.nickname}</span>`+"작가의 서재";
-    contentHeader.innerHTML = `<img id="reviewWriteBtn" src="file-write.png" alt="리뷰 작성 아이콘">`;
+    contentHeader.innerHTML = `<img id="writeBtn" src="file-write.png" alt="리뷰 작성 아이콘">`;
   }else{
     userName.innerHTML = `<span class="nameBold">${data.data.name}</span>`+"의 서재";
     contentHeader.innerHTML = `<img id="moreSeeBtn" src="more.png" alt="더보기 아이콘">`;
   }
 
 }
+
+// 작가 책 작성, 유저 더보기 클릭
+contentHeader.onclick = async() => {
+  const data = await axios.post("/v3/mainhome/cookieInfo", {cookieJwt});
+  if(data.data.nickname){
+    location.href = "../bookAdd";
+  }else{
+    alert("하이");
+    return;
+  }
+};
+
+// itemContainer에 책 item append로 추가하기
+const itemContainerElem = document.getElementById("itemContainer");
+
+// 이 아래 전체를 for문
+for(let i = 0; i<3; i++){
+  const itemDiv = document.createElement("div");
+  itemDiv.classList.add("item");
+
+  const imgBoxDiv = document.createElement("div");
+  imgBoxDiv.classList.add("img_box");
+  const bookInfoDiv = document.createElement("div");
+  bookInfoDiv.classList.add("book_info");
+
+  const imgElem = document.createElement("img");
+  imgElem.setAttribute("src", "https://cover.millie.co.kr/service/cover/37781287/d022459b4948433d9593fb9ce8e26a07.jpg?w=145&q=80");
+  imgElem.setAttribute("alt", "책 이미지");
+
+  const bookTitleDiv = document.createElement("div");
+  bookTitleDiv.classList.add("book_title");
+  bookTitleDiv.innerHTML = `책 이름`;
+  const authorInfoDiv = document.createElement("div");
+  authorInfoDiv.classList.add("author_info");
+  authorInfoDiv.innerHTML = `작가작가작가작가작가작가작가작가작가작가작가작가`;
+
+  bookInfoDiv.append(bookTitleDiv);
+  bookInfoDiv.append(authorInfoDiv);
+  imgBoxDiv.append(imgElem);
+  itemDiv.append(imgBoxDiv);
+  itemDiv.append(bookInfoDiv);
+  itemContainerElem.append(itemDiv);
+}
+
+
